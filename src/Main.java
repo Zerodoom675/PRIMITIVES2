198883b2ef1da23f3d07aaaea089cc674cc4f0e6
Zerodoom <zerodoom675@mail.ru>
2023-09-24 20:00:57 +0300

    second task

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-
+        int balance = 125;
+        short promotion = 1000;
+        int bonus;
+        int refill = 2820;
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
 
+        if (refill > promotion) {
+            bonus = refill / 100;
+            balance = balance + bonus + refill;
+            System.out.println("Ваш баланс " + balance + " начислено бонусных рубрей " + bonus);
+        } else {
+            balance = balance + refill;
+            System.out.println("Ваш баланс " + balance + " начислено бонусных рубрей 0");
+        }
         // Условным оператором проверяете, превысила ли
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
